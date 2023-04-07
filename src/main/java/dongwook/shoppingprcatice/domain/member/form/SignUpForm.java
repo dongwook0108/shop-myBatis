4af4c9899cac225e8f10b08350c6eafca0d0b30a
dongwook <kdw950108@gmail.com>
2023-04-08 00:21:32 +0900

    refactor : 상수 클래스로 변경

--- src/main/java/dongwook/shoppingprcatice/domain/member/form/SignUpForm.java
+++ src/main/java/dongwook/shoppingprcatice/domain/member/form/SignUpForm.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.Pattern;
 @Data
 public class SignUpForm {
 
+    public static final String EMAIL_REGEXP_CHECK_PATTERN = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+
     @NotEmpty
     @Length(min = 2, max = 10, message = "username은 2글자 이상 10글자 이하로 입력해 주세요" )
     private String username;
@@ -16,7 +18,7 @@ public class SignUpForm {
     @Length(min = 10, max = 10, message = "전화번호는 10자리 이상 11자리 이하로 입력해 주세요" )
     private String phoneNumber;
     @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일이 아닙니다.")
+    @Pattern(regexp = EMAIL_REGEXP_CHECK_PATTERN, message = "올바른 이메일이 아닙니다.")
     private String email;
     @NotEmpty(message = "공백일 수 없습니다")
     private String zipcode;
