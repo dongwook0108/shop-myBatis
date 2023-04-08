6b4d5690406894aa4ed7aeee02ea599820cbe63e
dongwook <kdw950108@gmail.com>
2023-04-08 20:08:56 +0900

    Add : H2 Db

--- src/main/java/dongwook/shoppingprcatice/domain/member/mapper/MemberMapper.java
+++ src/main/java/dongwook/shoppingprcatice/domain/member/mapper/MemberMapper.java
@@ -8,11 +8,11 @@ import java.util.List;
 @Mapper
 public interface MemberMapper {
 
-    @Insert(value = "INSERT INTO MEMBER(username, phone, email, zipcode, password, address, addressDetail) " +
+    @Insert(value = "INSERT INTO MEMBER(username, phoneNumber, email, zipcode, password, address, addressDetail) " +
             "VALUES(#{member.username}, #{member.phoneNumber},#{member.email},#{member.zipcode},#{member.password},#{member.address},#{member.addressDetail})")
     void save(@Param(value = "member") Member member);
 
-    @Select(value = "SELECT EXISTS (SELECT 1 FROM MEMBER WHERE phone = #{phoneNumber})")
+    @Select(value = "SELECT EXISTS (SELECT 1 FROM MEMBER WHERE phoneNumber = #{phoneNumber})")
     boolean existsByPhone(@Param(value = "phoneNumber") String phoneNumber);
 
     @Select(value = "SELECT EXISTS (SELECT 1 FROM MEMBER WHERE email = #{email})")
@@ -21,14 +21,14 @@ public interface MemberMapper {
     @Results(id = "MemberMap", value = {
             @Result(property = "id", column = "member_id"),
             @Result(property = "username", column = "username"),
-            @Result(property = "phoneNumber", column = "phone"),
+            @Result(property = "phoneNumber", column = "phoneNumber"),
             @Result(property = "email", column = "email"),
             @Result(property = "zipcode", column = "zipcode"),
             @Result(property = "password", column = "password"),
             @Result(property = "address", column = "address"),
             @Result(property = "addressDetail", column = "addressDetail"),
     })
-    @Select(value = "SELECT member_id, username, phone, email, zipcode, password, address, addressDetail FROM MEMBER WHERE email = #{username}")
+    @Select(value = "SELECT member_id, username, phoneNumber, email, zipcode, password, address, addressDetail FROM MEMBER WHERE email = #{username}")
     Member findByEmail(@Param(value = "username") String username);
 
 }
