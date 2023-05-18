30a24107103e3948268afdbb29db945af8934f57
dongwook <kdw950108@gmail.com>
2023-05-18 22:11:16 +0900

    feat- product page -> 상세화면 1. product{id}

--- src/main/java/dongwook/shoppingpractice/controller/ProductController.java
+++ src/main/java/dongwook/shoppingpractice/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,5 +28,10 @@ public class ProductController {
         Product product = productService.findById(id);
         model.addAttribute("product", product);
         return "product/product-detail";
+    }
+
+    @PostMapping("/product/{id}")
+    public String order(@PathVariable Long id) {
+        return null;
     }
 }
