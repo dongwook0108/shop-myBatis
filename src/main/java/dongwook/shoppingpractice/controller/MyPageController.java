5f29f91b638558f242c33fb0e8a1b33daa09b17f
dongwook <kdw950108@gmail.com>
2023-05-20 18:55:59 +0900

    feat - 카테고리 findParentCategory 보여주기

--- src/main/java/dongwook/shoppingpractice/controller/MyPageController.java
+++ src/main/java/dongwook/shoppingpractice/controller/MyPageController.java
@@ -1,7 +1,10 @@
 package dongwook.shoppingpractice.controller;
 
+import dongwook.shoppingpractice.model.Category;
 import dongwook.shoppingpractice.model.member.CurrentMember;
 import dongwook.shoppingpractice.model.member.Member;
+import dongwook.shoppingpractice.service.CategoryService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +14,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class MyPageController {
 
+    private final CategoryService categoryService;
+
     @GetMapping("/my-info")
-    public String myInfo() {
+    public String myInfo(Model model) {
+
+        List<Category> categoryList = categoryService.findParentCategory();
+        model.addAttribute("categoryList", categoryList);
+
         return "member/my-info";
     }
 
     @GetMapping("/my-info-edit")
     public String myInfoEdit(@CurrentMember Member member, Model model) {
+
         model.addAttribute("member", member);
+
+        List<Category> categoryList = categoryService.findParentCategory();
+        model.addAttribute("categoryList", categoryList);
 
         return "member/my-info-edit";
     }
