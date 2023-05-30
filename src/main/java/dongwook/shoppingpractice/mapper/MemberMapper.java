72dc8e2d6871fab4332fbd94f4073d87f4c19e68
dongwook <hal0110@naver.com>
2023-05-30 21:56:39 +0900

    refactor - MemberMapper 리팩토링

--- src/main/java/dongwook/shoppingpractice/mapper/MemberMapper.java
+++ src/main/java/dongwook/shoppingpractice/mapper/MemberMapper.java
@@ -17,15 +17,17 @@ public interface MemberMapper {
 
     @ResultMap(value = "MemberMap")
     @Insert(value =
-            "INSERT INTO MEMBER(username, role, phone_number, email, zipcode, password, address, address_detail, active, created_date, created_by) "
-                    +
-                    "VALUES(#{member.username}, #{member.role},  #{member.phoneNumber},#{member.email},#{member.zipcode},#{member.password},#{member.address},#{member.addressDetail},#{member.active}, #{member.createdDate}, #{member.createdBy})")
+            "INSERT INTO member(username, role, phone_number, email, zipcode, password, address,"
+                    + " address_detail, active, created_date, created_by)"
+                    + " VALUES(#{member.username}, #{member.role},  #{member.phoneNumber},"
+                    + " #{member.email}, #{member.zipcode}, #{member.password}, #{member.address},"
+                    + " #{member.addressDetail}, #{member.active}, #{member.createdDate}, #{member.createdBy})")
     void save(@Param(value = "member") Member member);
 
-    @Select(value = "SELECT EXISTS (SELECT 1 FROM MEMBER WHERE phone_number = #{phoneNumber})")
+    @Select(value = "SELECT EXISTS (SELECT 1 FROM member WHERE phone_number = #{phoneNumber})")
     boolean existsByPhone(@Param(value = "phoneNumber") String phoneNumber);
 
-    @Select(value = "SELECT EXISTS (SELECT 1 FROM MEMBER WHERE email = #{email})")
+    @Select(value = "SELECT EXISTS (SELECT 1 FROM member WHERE email = #{email})")
     boolean existsByEmail(@Param(value = "email") String email);
 
     @Results(id = "MemberMap", value = {
@@ -43,39 +45,44 @@ public interface MemberMapper {
             @Result(property = "createdDate", column = "created_date"),
             @Result(property = "updatedBy", column = "updated_by")
     })
-    @Select(value = "SELECT * FROM MEMBER WHERE email = #{email}")
+    @Select(value = "SELECT * FROM member WHERE email = #{email}")
     Member findByEmail(@Param(value = "email") String email);
 
     @ResultMap(value = "MemberMap")
-    @Select(value = "SELECT * FROM MEMBER WHERE member_id = #{memberId}")
+    @Select(value = "SELECT * FROM member WHERE member_id = #{memberId}")
     Member findById(@Param(value = "memberId") Long memberId);
 
     @ResultMap(value = "MemberMap")
-    @Update(value = "UPDATE MEMBER SET username=#{member.username}, phone_number=#{member.phoneNumber}, email=#{member.email}, zipcode=#{member.zipcode}, address=#{member.address}, address_detail=#{member.addressDetail}, updated_date=#{member.updatedDate}, updated_by=#{member.updatedBy} WHERE member_id = #{member.id}")
+    @Update(value =
+            "UPDATE member SET username=#{member.username}, phone_number=#{member.phoneNumber},"
+                    + " email=#{member.email}, zipcode=#{member.zipcode}, address=#{member.address},"
+                    + " address_detail=#{member.addressDetail}, updated_date=#{member.updatedDate},"
+                    + " updated_by=#{member.updatedBy} WHERE member_id = #{member.id}")
     void updateMember(@Param(value = "member") Member member);
 
-//    --------------------------페이징 -------------------------
-
+    //    --------------------------페이징 -------------------------
     @Select(value = "SELECT count(*) as listCnt from member where active = true")
     int getCount();
 
     @Select(value = "SELECT count(*) as listCnt from member where email=#{email} and active = true")
     int getCountByEmail(@Param(value = "email") String email);
 
-    @Select(value = "SELECT * FROM member WHERE active = true ORDER BY member_id DESC LIMIT #{page.rowCount} OFFSET #{page.offset}")
+    @Select(value = "SELECT * FROM member WHERE active = true "
+            + "ORDER BY member_id DESC LIMIT #{page.rowCount} OFFSET #{page.offset}")
     List<Member> getListPage(@Param(value = "page") PaginationVo paginationVo);
 
 
-    @Select(value = "SELECT * FROM member where email like concat('%',#{email},'%') ORDER BY member_id DESC LIMIT #{page.rowCount} OFFSET #{page.offset}")
+    @Select(value = "SELECT * FROM member where email like concat('%',#{email},'%')"
+            + " ORDER BY member_id DESC LIMIT #{page.rowCount} OFFSET #{page.offset}")
     List<Member> getListPageByEmail(@Param(value = "page") PaginationVo paginationVo,
             @Param(value = "email") String email);
 
-    @Update(value = "UPDATE MEMBER SET active=#{member.active} WHERE member_id=#{member.id}")
+    @Update(value = "UPDATE member SET active=#{member.active} WHERE member_id=#{member.id}")
     void deleteMember(@Param(value = "member") Member member);
 
-    @Update(value = "UPDATE MEMBER SET username=#{member.username} WHERE member_id=#{member.id}")
+    @Update(value = "UPDATE member SET username=#{member.username} WHERE member_id=#{member.id}")
     void updateName(@Param(value = "member") Member member);
 
-    @Update(value = "UPDATE MEMBER SET phone_number=#{member.phoneNumber} WHERE member_id=#{member.id}")
+    @Update(value = "UPDATE member SET phone_number=#{member.phoneNumber} WHERE member_id=#{member.id}")
     void updatePhoneNumber(@Param(value = "member") Member member);
 }
