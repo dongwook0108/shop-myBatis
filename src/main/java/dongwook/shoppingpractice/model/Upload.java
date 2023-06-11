0589e2df931d862b61346448db80545796e35f9e
dongwook <hal0110@naver.com>
2023-06-11 15:10:12 +0900

    test - 업로드 테스트

--- src/main/java/dongwook/shoppingpractice/model/Upload.java
+++ src/main/java/dongwook/shoppingpractice/model/Upload.java
@@ -4,6 +4,7 @@ import dongwook.shoppingpractice.form.upload.UploadForm;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
