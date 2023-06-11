32f1c8bd60a3afb77059322a2fc74807ec4e1087
dongwook <hal0110@naver.com>
2023-06-11 15:12:59 +0900

    refactor - 필요없는 import 삭제

--- src/test/java/dongwook/shoppingpractice/service/UploadServiceTest.java
+++ src/test/java/dongwook/shoppingpractice/service/UploadServiceTest.java
@@ -1,12 +1,9 @@
 package dongwook.shoppingpractice.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import dongwook.shoppingpractice.form.upload.UploadForm;
-import dongwook.shoppingpractice.model.Upload;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
